8d573b9dff80a12c267c3795faac02c23ba17600
gurusha0208 <47274854+gurusha0208@users.noreply.github.com>
2022-07-07 12:54:37 +0800

    created config file

---
+++ ChannelEngine_ConsoleApp/Config.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChannelEngine_ConsoleApp
+{
+    public static class Config
+    {
+        public static string Status = "IN_PROGRESS";
+        public static int StockValue = 25;
+    }
+}
