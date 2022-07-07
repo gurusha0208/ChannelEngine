9af1be81e3cd521f0515914bdff53afbec14f04e
gurusha0208 <47274854+gurusha0208@users.noreply.github.com>
2022-07-07 12:55:20 +0800

    added files

---
+++ ChannelEngine_ConsoleApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using ChannelEngine_Domain.Model;
+using ChannelEngine_Repository;
+using ChannelEngine_Repository.Implementation;
+using ChannelEngine_Repository.Interface;
+using ChannelEngine_Service.Implementation;
+using ChannelEngine_Service.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChannelEngine_ConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IProductService, ProductService>()
+                .AddSingleton<IProductRepository, ProductRepository>()
+                .AddSingleton<Repository, Repository>()
+                .BuildServiceProvider();
+                    
+
+            var product = serviceProvider.GetService<IProductService>();
+            var orders = product.GetTopProducts(Config.Status);
+            DisplayOfTopFive(orders);
+            
+            Console.WriteLine("Please type the product number to Update Stock");
+
+            var line = Console.ReadLine().Replace(" ","").Replace("-","").ToLower();
+
+            while(!orders.Select(x => x.MerchantProductNo.ToLower().Replace("-","")).ToList().Contains(line))
+            {
+                Console.WriteLine("Please type a valid stock number");
+                line = Console.ReadLine().Replace(" ", "").Replace("-", "").ToLower();
+            }
+
+            var item = orders.Where(x => x.MerchantProductNo.ToLower().Replace("-","") == line).FirstOrDefault();
+            
+            var model = new Product()
+            {
+                MerchantProductNo = item.MerchantProductNo,
+                StockLocations = new StockLocation()
+                {
+                    StockLocationId = item.StockLocation.Id,
+                    Stock = Config.StockValue
+                }
+            };
+
+            var result = product.UpdateStock(model);
+            
+            if (result)
+            {
+                Console.WriteLine("Updated successfully");
+            }
+            else
+            {
+                Console.WriteLine("Error while Updating");
+            }
+
+            orders = product.GetTopProducts(Config.Status);
+            DisplayOfTopFive(orders);
+        }
+
+        public static void DisplayOfTopFive(List<Line> orders)
+        {
+            Console.WriteLine(String.Format("|{0,15}|{1,20}|{2,5}|", "Product No", "Gtin", "Qty"));
+            foreach (var itemLine in orders)
+            {
+                Console.WriteLine(String.Format("|{0,15}|{1,20}|{2,5}|", itemLine.MerchantProductNo, itemLine.Gtin, itemLine.Quantity));
+            }
+           
+        }
+    }
+}
